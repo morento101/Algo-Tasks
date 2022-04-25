4e2ee770deaab364796cfb007565121c98e30fe7
Misha86 <mpolishchuk86@gmail.com>
2022-04-25 21:49:03 +0300

    corrected tests files

--- tests/test_task_87.py
+++ tests/test_task_87.py
@@ -4,13 +4,14 @@ from tasks.task_87 import task_87
 
 
 class TestTask87(unittest.TestCase):
-    """ Tests for function task_87. """
-
-    def test_target_arg_zero(self):
+    """
+    Tests for function task_87.
+    """
+    def test_arg_is_zero(self):
         """
         Test that number is zero a ValueError exception is called.
         """
-        with self.assertRaises(ValueError):
+        with self.assertRaises(AssertionError):
             task_87(0, 1)
 
     def test_tail_size_one(self):
@@ -19,13 +20,15 @@ class TestTask87(unittest.TestCase):
         """
         self.assertEqual(task_87(112, 1), 2)
 
-    def test_task_87(self):
+    def test_valid(self):
         """
         Test that task_87 function returns correct result.
         """
         self.assertEqual(task_87(112, 2), 3)
 
     def test_one_of_args_has_incorrect_type(self):
-        """ Test when one of args is incorrect data type a TypeError exception is called. """
+        """
+        Test when one of args is incorrect data type a TypeError exception is called.
+        """
         self.assertRaises(TypeError, task_87, "5", 10)
         self.assertRaises(TypeError, task_87, 5, "10")
