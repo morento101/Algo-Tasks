8852d8ec3751b3577a92e0b8de4f7dd8ac6f78b4
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-04-26 16:51:48 +0300

    Added my task files

---
+++ tasks/task_108.py
@@ -0,0 +1,18 @@
+"""
+108 Given a natural number, find the smallest possible number which matches 
+pattern 2**r and is bigger than the given number
+"""
+
+from utils import is_natural_number
+
+
+def task_108(number: int) -> int:
+    """Return the smallest possible number that matches
+    pattern 2**r and is bigger than number
+    """
+    assert is_natural_number(number), "Wrong argument, must be natural integer"
+    r = 1
+    while True:
+        if 2**r > number:
+            return 2**r
+        r += 1
