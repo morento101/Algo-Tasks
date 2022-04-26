44b1187a250c8698708eec2acace80b10c59a8a7
therobotisnotatoy <pythonshumor@gmail.com>
2022-04-26 22:35:49 +0300

    Docstrings changed to single line representation Redundant functions removed Function name changed to corresponding task_xx

--- tasks/task_226.py
+++ tasks/task_226.py
@@ -1,10 +1,8 @@
 """226. Given natural numbers m, n. Get all natural common multiples, smaller m*n."""
 
 
-def get_natural_common_multiples(first_arg: int, second_arg: int) -> list[int]:
-    """
-    Returns all natural common multiples of first_arg & second_arg less than first_arg * second_arg
-    """
+def task_226(first_arg: int, second_arg: int) -> list[int]:
+    """Return all natural common multiples of first_arg & second_arg less than first_arg * second_arg."""
     limit = first_arg * second_arg
     suspect = second_arg
     result = []
@@ -13,24 +11,5 @@ def get_natural_common_multiples(first_arg: int, second_arg: int) -> list[int]:
         if suspect % first_arg == 0:
             result.append(suspect)
         suspect += second_arg
-
     return result
 
-
-def info() -> str:
-    """
-    Task & arguments description
-    """
-    info_string = ("Task 226. Function returns all natural common multiples of n & m less than n*m.\n"
-                   "Please enter n, m separated by space (or 'R' to return to main menu): "
-                   )
-
-    return info_string
-
-
-def run(n, m) -> list[int]:
-    """
-    Implementation of the task #226.
-    """
-
-    return get_natural_common_multiples(n, m)
