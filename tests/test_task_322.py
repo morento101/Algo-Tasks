78d8f0ee5a03276f4e6dd6813de5fab7008ca22f
therobotisnotatoy <pythonshumor@gmail.com>
2022-04-26 22:38:05 +0300

    Docstrings changed to single line representation Function name changed to corresponding task_xx

--- tests/test_task_322.py
+++ tests/test_task_322.py
@@ -1,108 +1,85 @@
-""" Test of task_322.py """
+"""Test of task_322.py."""
+
 from unittest import TestCase
 from unittest import main
 
-from tasks.task_322 import (get_divisors_list, get_number_with_largest_sum_of_divisors)
+from tasks.task_322 import (get_divisors_list, task_322)
 
 
 class TestGetDivisorsList(TestCase):
-    """
-    Tests for function: get_divisors_list
-    """
+    """Tests for function: get_divisors_list."""
 
     def test_with_zero_argument(self):
-        """
-        Test with 0 as function argument
-        """
+        """Test with 0 as function argument."""
         with self.assertRaises(AssertionError) as test_exception:
             get_divisors_list(0)
         self.assertEqual("Number should be greater than 0", test_exception.exception.args[0])
 
     def test_with_negative_argument(self):
-        """
-        Test with negative number as function argument
-        """
+        """Test with negative number as function argument."""
         with self.assertRaises(AssertionError) as test_exception:
             get_divisors_list(-1)
         self.assertEqual("Number should be greater than 0", test_exception.exception.args[0])
 
     def test_negative_result(self):
-        """
-        Test with negative result expectation
-        """
+        """Test with negative result expectation."""
         self.assertNotEqual(get_divisors_list(16), [])
         self.assertNotEqual(get_divisors_list(133), [1])
 
     def test_positive_result(self):
-        """
-        Test with positive result expectation
-        """
+        """Test with positive result expectation."""
         self.assertEqual(get_divisors_list(1), [1])
         self.assertEqual(get_divisors_list(131), [1, 131])
         self.assertEqual(get_divisors_list(16), [1, 2, 4, 8, 16])
         
 
 class TestGetNumberWithLargestSumOfDivisors(TestCase):
-    """
-    Tests for function: get_number_with_largest_sum_of_divisors
-    """
+    """Tests for function: get_number_with_largest_sum_of_divisors."""
 
     def test_without_arguments(self):
-        """
-        Test without function argument
-        """
-        self.assertRaises(TypeError, get_number_with_largest_sum_of_divisors)
-        self.assertRaises(TypeError, get_number_with_largest_sum_of_divisors, 1)
+        """Test without function argument."""
+        self.assertRaises(TypeError, task_322)
+        self.assertRaises(TypeError, task_322, 1)
 
     def test_with_zero_argument(self):
-        """
-        Test with 0 as function argument
-        """
+        """Test with 0 as function argument."""
         with self.assertRaises(AssertionError) as test_exception:
-            get_number_with_largest_sum_of_divisors(0, 55)
+            task_322(0, 55)
         self.assertEqual("Number should be greater than 0", test_exception.exception.args[0])
 
     def test_with_negative_arguments(self):
-        """
-        Test with negative number as function argument
-        """
+        """Test with negative number as function argument."""
         with self.assertRaises(AssertionError) as test_exception:
-            get_number_with_largest_sum_of_divisors(-1, 55)
+            task_322(-1, 55)
         self.assertEqual("Number should be greater than 0", test_exception.exception.args[0])
 
         with self.assertRaises(AssertionError) as test_exception:
-            get_number_with_largest_sum_of_divisors(-55, -1)
+            task_322(-55, -1)
         self.assertEqual("Number should be greater than 0", test_exception.exception.args[0])
 
     def test_with_reversed_arguments(self):
-        """
-        Test with reversed range as function arguments
-        """
+        """Test with reversed range as function arguments."""
         with self.assertRaises(AssertionError) as test_exception:
-            get_number_with_largest_sum_of_divisors(55, 1)
+            task_322(55, 1)
         self.assertEqual("Second (b) argument should be bigger than first (a)",
                          test_exception.exception.args[0]
                          )
 
         with self.assertRaises(AssertionError) as test_exception:
-            get_number_with_largest_sum_of_divisors(-1, -55)
+            task_322(-1, -55)
         self.assertEqual("Second (b) argument should be bigger than first (a)",
                          test_exception.exception.args[0]
                          )
 
     def test_negative_result(self):
-        """
-        Test with negative result expectation
-        """
-        self.assertNotEqual(get_number_with_largest_sum_of_divisors(1, 10), 5)
-        self.assertNotEqual(get_number_with_largest_sum_of_divisors(111, 222), 100)
+        """Test with negative result expectation."""
+        self.assertNotEqual(task_322(1, 10), 5)
+        self.assertNotEqual(task_322(111, 222), 100)
 
     def test_positive_result(self):
-        """
-        Test with positive result expectation
-        """
-        self.assertEqual(get_number_with_largest_sum_of_divisors(1, 10), 8)
-        self.assertEqual(get_number_with_largest_sum_of_divisors(111, 222), 106)
+        """Test with positive result expectation."""
+        self.assertEqual(task_322(1, 10), 8)
+        self.assertEqual(task_322(111, 222), 106)
 
 
 if __name__ == "__main__":
