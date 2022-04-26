236920016717a0d3025df5012a45bff6c2ba2561
Misha86 <mpolishchuk86@gmail.com>
2022-04-26 10:58:01 +0300

    changed docstring run_tests

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,10 @@
-""" Test runner for tests from tasks/tests/ dir. """
+"""Test runner for tests from tasks/tests/ dir."""
 import os
 import unittest
 
 
 def suite(tests_dir='tests', pattern='test_task_*.py'):
-    """
-    Add all test from tasks/tests/ dir to TestSuite.
-    """
+    """Add all test from tasks/tests/ dir to TestSuite."""
     this_dir = os.path.join(os.path.dirname(__file__), tests_dir)
     loader = unittest.TestLoader()
     suite_tests = unittest.TestSuite()
