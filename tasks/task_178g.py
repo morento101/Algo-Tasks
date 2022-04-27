3f1df4466cd43c2bfd1af48bd659e9b26e580ba9
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 09:08:44 +0300

    Update task_178g.py

--- tasks/task_178g.py
+++ tasks/task_178g.py
@@ -1,17 +1,16 @@
 """Module with task 178d"""
 
 
-def task178g(n: int, arr: list) -> int:
-    """
-    Return number that satisfy a following condition:
+def task178g(num: int, arr: list) -> int:
+    """ Return number that satisfy a following condition:
     l[elem] < (l[elem + 1] + l[elem - 1]) / 2:
     """
-    if not isinstance(n, int):
+    if not isinstance(num, int):
         raise TypeError("Argument should be integer!")
-    if n <= 0:
+    if num <= 0:
         raise ValueError("Argument is not natural number!")
-    c = 0
-    for elem in range(1, n - 1):
+    count = 0
+    for elem in range(1, num - 1):
         if arr[elem] < (arr[elem + 1] + arr[elem - 1]) / 2:
-            c += 1
-    return c
+            count += 1
+    return count
