ae3efce7b8e3b711f74bb9104f5cf4dba75045b9
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 09:11:09 +0300

    Update task_555.py

--- tasks/task_555.py
+++ tasks/task_555.py
@@ -1,16 +1,17 @@
 """Module with task 555"""
+
 from math import factorial as fac
 
 
-def task555(n: int) -> list:
+def task555(num: int) -> list:
+    """ Build pascal's triangle
+    with n floors
     """
-    Build pascal's triangle with n floors
-    """
-    if not isinstance(n, int):
+    if not isinstance(num, int):
         raise TypeError("Argument should be integer!")
-    if n <= 0:
+    if num <= 0:
         raise ValueError("Argument is not natural number!")
 
-    return [[int((fac(n) / (fac(k) * fac(n - k)))) for k in range(0, n + 1)] for n in range(0, n)]
+    return [[int((fac(num) / (fac(k) * fac(num - k)))) for k in range(0, num + 1)] for num in range(0, num)]
 
 
