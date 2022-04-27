33377f55c7832737a9cbdc43ee2f12a4ce83dab9
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 09:15:19 +0300

    Update task_555.py

--- tasks/task_555.py
+++ tasks/task_555.py
@@ -12,5 +12,3 @@ def task555(num: int) -> list:
     if num <= 0:
         raise ValueError("Argument is not natural number!")
     return [[int((fac(num) / (fac(k) * fac(num - k)))) for k in range(0, num + 1)] for num in range(0, num)]
-
-
