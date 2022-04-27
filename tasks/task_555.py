cf2ce027f4f4809283cc2aa1fd243f5e9f2af0bd
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 12:42:56 +0300

    Update task_555.py

--- tasks/task_555.py
+++ tasks/task_555.py
@@ -2,13 +2,12 @@
 
 from math import factorial as fac
 
+from tasks.utils import is_natural_number
+
 
 def task555(num: int) -> list:
     """ Build pascal's triangle
     with n floors
     """
-    if not isinstance(num, int):
-        raise TypeError("Argument should be integer!")
-    if num <= 0:
-        raise ValueError("Argument is not natural number!")
+    assert is_natural_number(num), "The number should be natural"
     return [[int((fac(num) / (fac(k) * fac(num - k)))) for k in range(0, num + 1)] for num in range(0, num)]
