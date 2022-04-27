41de9862ea4aec0354aed394dcb2eb9f91f12b80
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 09:21:25 +0300

    Update test_task_560.py

--- tests/test_task_560.py
+++ tests/test_task_560.py
@@ -1,22 +1,18 @@
 """Module with tests for task 560"""
-from unittest import TestCase
+
+from unitest import TestCase
+
 from tasks.task_560 import task_560
 
 
 class TestTask560(TestCase):
-    """
-    Tests for task 560
-    """
+    """Tests for task 560"""
     def test_valid(self):
-        """
-        Test with valid result
-        """
+        """ Test with valid result """
         test_res = ['220 284']
         self.assertEqual(task_560(), test_res)
 
     def test_not_valid(self):
-        """
-        Test with not valid result
-        """
+        """ Test with not valid result """
         test_res = ['221 284']
         self.assertNotEqual(task_560(), test_res)
