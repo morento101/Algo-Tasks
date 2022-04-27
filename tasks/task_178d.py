8d8fd1d5ccf920de54b0c9dd9e5e74b73c3119cb
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 09:06:36 +0300

    Update task_178d.py

--- tasks/task_178d.py
+++ tasks/task_178d.py
@@ -1,10 +1,10 @@
 """Module with task 178d"""
+
 from math import factorial as fac
 
 
 def task178d(n: int, arr: list) -> int:
-    """
-    Return number that satisfy a following condition:
+    """ Return number that satisfy a following condition:
     2**k< ak < k!
     """
     if not isinstance(n, int):
