1570d4fe2bcead497851071655ad219dca2aa88b
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-04-27 12:40:49 +0300

    Update task_178g.py

--- tasks/task_178g.py
+++ tasks/task_178g.py
@@ -1,14 +1,13 @@
 """Module with task 178d"""
 
+from tasks.utils import is_natural_number
+
 
 def task178g(num: int, arr: list) -> int:
     """ Return number that satisfy a following condition:
     l[elem] < (l[elem + 1] + l[elem - 1]) / 2:
     """
-    if not isinstance(num, int):
-        raise TypeError("Argument should be integer!")
-    if num <= 0:
-        raise ValueError("Argument is not natural number!")
+    assert is_natural_number(num), "The number should be natural"
     count = 0
     for elem in range(1, num - 1):
         if arr[elem] < (arr[elem + 1] + arr[elem - 1]) / 2:
