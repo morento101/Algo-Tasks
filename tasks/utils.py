c0b2e9e833677b11c11a820b3e94ae9caecdd375
shv833 <87500387+shv833@users.noreply.github.com>
2022-04-27 11:51:02 +0300

    Update utils.py

--- tasks/utils.py
+++ tasks/utils.py
@@ -2,15 +2,11 @@
 
 
 def is_natural_number(number: int) -> bool:
-    """
-    Returns True if the number is natural
-    """
+    """Returns True if the number is natural."""
     return isinstance(number, int) and number > 0
 
 def has_int_in_list(array: list[int]) -> bool:
-    """
-    Returns True if the list has integer elements.
-    """
+    """Returns True if the list has integer elements."""
     tmp: bool
     for i in array:
         if not isinstance(i, int):
