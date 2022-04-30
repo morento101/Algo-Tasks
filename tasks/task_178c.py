c32b0e4a0270f0cdefffc8580d9af8c73a7c6e5c
Yaroslav Kovaliv <80165970+morento101@users.noreply.github.com>
2022-04-30 14:59:17 +0300

    Update task_178c.py

--- tasks/task_178c.py
+++ tasks/task_178c.py
@@ -13,7 +13,7 @@ def task_178c(numbers: Sequence[int]) -> int:
     count = 0
 
     for number in numbers:
-        assert isinstance(number, int), "Numbers in sequene must be int or float"
+        assert isinstance(number, int), "Numbers in sequene must be intt"
         assert number >= 0, f"Can't calculate square root from negative number {number}"
         number_root = number ** .5
         if not (number_root % 1) and not (number_root % 2):
