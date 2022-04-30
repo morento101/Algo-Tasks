ec4276506d29a88f5c4f9160c19aed0a2f57a965
Yaroslav Kovaliv <80165970+morento101@users.noreply.github.com>
2022-04-30 15:09:39 +0300

    Simplified type check

--- tasks/task_178b.py
+++ tasks/task_178b.py
@@ -9,10 +9,11 @@ def task_178b(numbers: Sequence[int]) -> int:
     """Return amount of numbers from sequence which are 
     multiples of three but not multiples of five
     """
-    assert type(numbers) in (tuple, set, list), "Sequence must be tuple, set or list"
+    assert isinstance(numbers, (tuple, set, list), "Sequence must be tuple, set or list"
     count = 0
 
     for number in numbers:
+        assert isinstance(number, int), "Numbers in sequene must be int"
         if not number % 3 and number % 5:
             count += 1
 
