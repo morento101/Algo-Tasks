3a0cbd2f038a32366fc2b9339835b1c638fc14af
Yaroslav Borysko <91908798+YaroslavBorysko@users.noreply.github.com>
2022-05-03 12:45:11 +0300

    Update task_178d.py

--- tasks/task_178d.py
+++ tasks/task_178d.py
@@ -8,7 +8,7 @@ def task_178d(num: int, arr: list) -> int:
     """ Return number that satisfy a following condition:
     2**k< ak < k!
     """
-     assert is_natural_number(num), "The number should be natural"
+    assert is_natural_number(num), "The number should be natural"
     count = 0
     for elem in range(num):
         if arr[elem] > 2**elem and arr[elem] < fac(elem):
