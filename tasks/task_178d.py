63a881c34e68034a196089d585f9deab781ee5f7
YaroslavBorysko <yaroslavborysko@gmail.com>
2022-05-04 20:46:21 +0300

    added *args

--- tasks/task_178d.py
+++ tasks/task_178d.py
@@ -1,14 +1,15 @@
 """Module with task 178d"""
 
 from math import factorial as fac
-from tasks.utils import is_natural_number
+from tasks.utils import has_int_in_list
 
 
 def task_178d(*arr: tuple) -> int:
     """ Return number that satisfy a following condition:
     2**k< ak < k!
     """
-    # assert is_natural_number(num), "The number should be natural"
+    assert has_int_in_list(arr), "The number should be natural"
+
     count = 0
     for elem in range(len(arr)):
         if arr[elem] > 2**elem and arr[elem] < fac(elem):
