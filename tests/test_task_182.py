cc405b34d5b4927155e16b32022648f0ecff4c50
YaroslavBorysko <yaroslavborysko@gmail.com>
2022-05-05 18:45:10 +0300

    added *args

--- tests/test_task_182.py
+++ tests/test_task_182.py
@@ -10,22 +10,19 @@ class TestTask182(TestCase):
     
     def test_assert_equal(self):
         """ Test with positive numbers in sequence """
-        test_n = 5
-        test_list = [6, 35, 40, 97, 21]
-        self.assertEqual(task_182(test_n, test_list), (1, 40))
+        test_list = (6, 35, 40, 97, 21)
+        self.assertEqual(task_182(*test_list), (1, 40))
 
     def test_assert_raises(self):
         """ Test with string in sequence """
-        self.assertRaises(AssertionError, task_182, "3", "[2, 3, 4]")
+        self.assertRaises(AssertionError, task_182, "[2, 3, 4]")
 
     def test_with_negative_arr(self):
         """ Test with negative numbers in sequence """
-        test_n = 1
         test_list = [-6]
-        self.assertEqual(task_182(test_n, test_list), (0, 0))
+        self.assertEqual(task_182(*test_list), (0, 0))
 
     def test_with_zero(self):
         """ Test with zero """
-        test_n = 0
         test_list = []
-        self.assertRaises(AssertionError, task_182, test_n, test_list)
+        self.assertRaises(AssertionError, task_182, test_list)
