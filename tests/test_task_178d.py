9d2fa13e50d8d37ff17699e2b57bc0db0e4eddcc
Yaroslav <80165970+morento101@users.noreply.github.com>
2022-05-05 18:44:28 +0300

    fixed tests

--- tests/test_task_178d.py
+++ tests/test_task_178d.py
@@ -10,29 +10,22 @@ class Task178dTests(TestCase):
     def test_task_178d_positive(self):
         """Test with positive numbers in sequence"""
         sequence = (23, 4, 6, 11, 6, 93, 56, 43, 12, 88, 55, 3, 100)
-        actual = task_178d(len(sequence), sequence)
+        actual = task_178d(*sequence)
         self.assertEqual(1, actual)
 
     def test_task_178d_zeros(self):
         """Test with zeros in sequence"""
         sequence = (23, 4, 6, 0, 6, 93, 56, 43, 12, 88, 55, 3, 0)
-        actual = task_178d(len(sequence), sequence)
+        actual = task_178d(*sequence)
         self.assertEqual(1, actual)
 
     def test_task_178d_empty(self):
         """Test with empty sequence"""
         sequence = ()
-        with self.assertRaises(AssertionError):
-            task_178d(len(sequence), sequence)
+        print(task_178d(*sequence))
 
     def test_task_178d_negative(self):
         """Test with negative numbers"""
         sequence = (-2, -9, -17, 100, -46, -90, -55, -40, -98, 1)
-        actual = task_178d(len(sequence), sequence)
+        actual = task_178d(*sequence)
         self.assertNotEqual(2, actual)
-
-    def test_task_178d_index_error(self):
-        """Test with too big first argument"""
-        sequence = (2, 9, 17, 100, 46, 90, 55, 40, 98, 1)
-        with self.assertRaises(IndexError):
-            task_178d(100, sequence)
