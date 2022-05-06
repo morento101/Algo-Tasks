b44fc13ed0e0a7394e7e088f97a1effecf84654e
Dmytro Tymoshchenko <dietrich.kreze@gmail.com>
2022-05-07 00:43:51 +0300

    New menu version

--- menu.py
+++ menu.py
@@ -2,98 +2,149 @@
 
 from inspect import signature
 from sys import exit
+from os import system
 
 from tasks.tasks import *
 
 
-data_dict = locals()
+data_dict = {key: func for key, func in locals().items()
+             if key.startswith("task_")}
+
+logo_str = "     __   __     ___   __   ____   __   ____  __ _  ____ \n" + \
+           "    / _\ (  )   / __) /  \ (_  _) / _\ / ___)(  / )/ ___)\n" + \
+           "   /    \/ (_/\( (_ \(  O )  )(  /    \\\\___ \ )  ( \___ \\\n" + \
+           "   \_/\_/\____/ \___/ \__/  (__) \_/\_/(____/(__\_)(____/"
+
+
+def pretify_func_name(func_name: str) -> str:
+    """
+    Make name of the function prettier
+    """
+    return func_name[5:]
 
 
 def pretify_doc(doc: str) -> str:
     """
-    This makes code beautiful
+    Make description of the function prettier
     """
     if doc[-1] == '\n':
-        return doc
-    return doc + '\n'
+        return doc.replace("    ", "")
+    return doc.replace("    ", "") + '\n'
+
+
+def clear_screen() -> None:
+    """
+    Clear screen from symbols
+    """
+    system('cls||clear')
+
+
+def print_logo() -> None:
+    """
+    Print app logo
+    """
+    print(logo_str)
+    print("*" * 60)
+
+
+def print_task_desc(func) -> None:
+    """
+    Print task description
+    """
+    print_logo()
+    print(pretify_doc(func.info))
+
+
+def print_result(result) -> None:
+    """
+    Print answer of the function
+    """
+    if not result:
+        print("Result: False")
+    elif isinstance(result, int):
+        print("Result:", result)
+    else:
+        print("Result:", *result)
+
+
+def print_menu(data_dict: dict) -> None:
+    """
+    Print the main menu with logo
+    """
+    print_logo()
+    print("Choose one of the available tasks:")
+    for key, value in enumerate(data_dict, 1):
+        print(pretify_func_name(value), end=" \t ")
+        if not key % 7:
+            print()
+    print("\nOr type 'exit' or 'e' to terminate app.")
+
 
 
 def main(data_dict: dict):
     """
-    Menu implementation
+    Main function for menu implementation
     """
-    # Dictionary of tasks. Key - task number, value - function of that task
-    tasks_dict = data_dict
 
-    # Main loop
     while True:
+        clear_screen()
+        print_menu(data_dict)
 
-        print("Type 'exit' to stop program execution")
-        # Print available tasks
-        print("*" * 24, "MAIN MENU", "*" * 25)
-        print("Available tasks: ")
-        for key, value in enumerate(tasks_dict):
-            if value.startswith("task_"):
-                print(value, end=", ")
-                if not key % 5:
-                    print()
-        print("\n")
-        print("*" * 60)
-
-        try:
-            task_number = input("Input task`s number: ")
-            # Exit from main loop
-            if task_number == "exit":
-                exit()
-
-            # Get the module by input
-            func = tasks_dict[f"task_{task_number}"]
-
-        except KeyError as ex:
-            print(f"Input correct task`s number, task {ex} does not exist: ")
-            continue
-
-        # Task loop
         while True:
+
             try:
-                # Get number of arguments needed for task run() function
-                expected_args_count = len(signature(func).parameters)
-                if expected_args_count < 1:
-                    # Show module info & call function without arguments
-                    print("-" * 60)
-                    print(func.info.replace("    ", ""))
-                    print("-" * 60)
-                    print("Result: ", func())
-                    break
-                else:
-                    # Show module info & get arguments (or "exit" key)
-                    print("-" * 60)
-                    print("Type 'return' to return to the main menu")
-                    user_input = input(pretify_doc(
-                        func.info.replace("    ", "")))
-                    # Exit from task loop
+                user_input = input(">>> ")
 
-                    if user_input == "return":
-                        break
-                    
-                    if user_input == "exit":
-                        exit()
-                    # Get arguments (you validation function could be here)
-                    args = [int(arg) for arg in user_input.split(" ")
-                            if (arg.isnumeric() and (int(arg) > 0))]
-                    # Validate number of parameters
-                    # assert(expected_args_count == len(args))
+                if user_input == "e" or user_input == "exit":
+                    exit()
 
-                    print("-" * 60)
+                func = data_dict[f"task_{user_input}"]
+                break
 
-                    # Get the result
-                    print("Result: ", func(*args))
-
-            except (TypeError, AssertionError):
-                print("-" * 60)
-                print(f"The type or number of arguments is not suitable"
-                      f" for this task. Pleas try again.")
+            except KeyError as ex:
+                if user_input:
+                    print(f"Input correct task`s number, task {ex} does not exist: ")
                 continue
+
+        while True:
+            clear_screen()
+            expected_args_count = len(signature(func).parameters)
+            if expected_args_count < 1:
+                print_task_desc(func)
+                print_result(func())
+                print("\nPress any key to return back")
+                input(">>> ")
+                break
+            else:
+                while True:
+                    print_task_desc(func)
+                    print("Print the arguments to get the answer. 'return'")
+                    print("or 'r' will bring you back to the menu.")
+                    print("Or type 'exit' or 'e' to terminate app.\n")
+
+                    while True:
+                        try:
+                            user_input = input(">>> ")
+
+                            if user_input == "exit" or user_input == "e":
+                                exit()
+
+
+                            args = [int(arg) for arg in user_input.split(" ")
+                                    if (arg.isnumeric() and (int(arg) > 0))]
+                            print_result(func(*args))
+
+                        except (TypeError, AssertionError):
+                            if user_input:
+                                print(f"The type or number of arguments is not suitable"
+                                f" for this task. Pleas try again.")
+                            continue
+
+                        finally:
+                            if user_input == "return" or user_input == "r":
+                                break
+                    break
+                break
 
 
 if __name__ == "__main__":
