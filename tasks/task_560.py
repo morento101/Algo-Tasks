b5247a14812397ea2c4fdb09dcc0a847443454c1
shv833 <volodya.shyra@gmail.com>
2022-05-18 15:32:47 +0300

    Renamed variables in task_323 & task_560

--- tasks/task_560.py
+++ tasks/task_560.py
@@ -3,26 +3,29 @@ Two natural numbers are called friendly if each of them is equal
 to the sum of all divisors of the other, except for this number itself.
 """
 
+
 def task_560() -> list:
     """Return all pairs of friendly numbers between 200 and 300."""
     tmp = 0
-    array = []
+    pairs_of_friendly_nums = []
     for k in range(200, 301):
         if k != tmp:
-            sum1 = 0
+            sum_of_divisor_1num = 0
             for i in range(1, k):
                 if k % i == 0:
-                    sum1 += i
+                    sum_of_divisor_1num += i
 
-            sum2 = 0
-            for j in range(1, sum1):
-                if sum1 % j == 0:
-                    sum2 += j
+            sum_of_divisor_2num = 0
+            for j in range(1, sum_of_divisor_1num):
+                if sum_of_divisor_1num % j == 0:
+                    sum_of_divisor_2num += j
 
-            if sum2 == k != sum1:
-                array.append(str(k) + ' ' + str(sum1))
-                tmp = sum1
+            if sum_of_divisor_2num == k != sum_of_divisor_1num:
+                pairs_of_friendly_nums.append(
+                    str(k) + ' ' + str(sum_of_divisor_1num))
+                tmp = sum_of_divisor_1num
 
-    return array
+    return pairs_of_friendly_nums
+
 
 task_560.info = __doc__
